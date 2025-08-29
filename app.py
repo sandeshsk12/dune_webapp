a91749f88310f94002a78e32e2bc6e7e60af7454
sandeshsk12 <sksandeshkandagal@gmail.com>
2025-08-29 20:35:22 +0530

    changed start

--- app.py
+++ app.py
@@ -101,5 +101,6 @@ def download():
     )
 
 if __name__ == "__main__":
-    # Run: python app.py  -> http://127.0.0.1:5000
-    app.run(debug=True)
+    import os
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=False)
+
